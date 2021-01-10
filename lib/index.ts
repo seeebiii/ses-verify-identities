1f569732f4737075826164b35706c3b7e746c7be
seeebiii <info@sebastianhesse.de>
2021-01-10 11:44:26 +0100

    Initial commit

---
+++ lib/index.ts
@@ -0,0 +1,14 @@
+import * as cdk from '@aws-cdk/core';
+
+export interface SesVerifyIdentitiesProps {
+  // Define construct properties here
+}
+
+export class SesVerifyIdentities extends cdk.Construct {
+
+  constructor(scope: cdk.Construct, id: string, props: SesVerifyIdentitiesProps = {}) {
+    super(scope, id);
+
+    // Define construct contents here
+  }
+}
