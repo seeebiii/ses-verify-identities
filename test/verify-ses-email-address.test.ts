2315e6daa0fc0c88b13b06a9774e8dcd0ed73f7b
Patrick Florek <patrick.florek@gmail.com>
2022-10-17 19:36:16 +0200

    fix(email): retain on removal
    Fixes https://github.com/seeebiii/ses-verify-identities/issues/506

--- test/verify-ses-email-address.test.ts
+++ test/verify-ses-email-address.test.ts
@@ -1,5 +1,6 @@
 
 import * as cdk from 'aws-cdk-lib';
+import { RemovalPolicy } from 'aws-cdk-lib';
 import { Match, Template } from 'aws-cdk-lib/assertions';
 import { VerifySesEmailAddress } from '../src';
 
@@ -7,9 +8,11 @@ test('ensure custom resource exists to verify email address', () => {
   const app = new cdk.App();
   const stack = new cdk.Stack(app, 'TestStack');
   const emailAddress = 'hello@example.org';
+  const removalPolicy = RemovalPolicy.DESTROY;
 
   new VerifySesEmailAddress(stack, 'VerifyExampleEmail', {
     emailAddress: emailAddress,
+    removalPolicy,
   });
 
   // Then
@@ -46,10 +49,12 @@ test('ensure custom resource exists in a custom region to verify email address',
   const stack = new cdk.Stack(app, 'TestStack');
   const emailAddress = 'hello@example.org';
   const region = 'us-east-1';
+  const removalPolicy = RemovalPolicy.DESTROY;
 
   new VerifySesEmailAddress(stack, 'VerifyExampleEmail', {
     emailAddress: emailAddress,
     region,
+    removalPolicy,
   });
 
   // Then
